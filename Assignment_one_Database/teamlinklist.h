0ffc1a62ac7c97dfdeec28c15a326cff69f533a2
ibna zia rafi <izrafi.au@gmail.com>
2025-04-17 04:49:21 +1000

    linklist structure for team has been created

--- Assignment_one_Database/teamlinklist.h
+++ Assignment_one_Database/teamlinklist.h
@@ -1,1 +1,22 @@
-#pragma once
+
+#ifndef TEAM_H
+#define TEAM_H
+
+#include "playerLinklist.h"
+
+typedef struct teamNode {
+    char* teamName;
+    PlayerList players;
+    struct teamNode* next;
+} TeamNode, * TeamNodePtr;
+
+typedef struct teamList {
+    TeamNodePtr head;
+} TeamList;
+
+// Function declarations
+TeamNodePtr create_team(const char* name);
+void add_team(TeamList* list, TeamNodePtr team);
+void print_teams(const TeamList* list);
+
+#endif
