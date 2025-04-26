054d000082f93eafd915e49b5394607f933f7f2d
ibna zia rafi <izrafi.au@gmail.com>
2025-04-26 22:48:54 +1000

    PlayerBST header file has been created

---
+++ Assignment_one_Database/playerBST.h
@@ -0,0 +1,18 @@
+#ifndef PLAYERBST_H
+#define PLAYERBST_H
+
+typedef struct playerBSTNode {
+    char* name;
+    char* position;
+    int age;
+    float contractValue;
+    struct playerBSTNode* left;
+    struct playerBSTNode* right;
+} PlayerBSTNode, * PlayerBSTNodePtr;
+
+PlayerBSTNodePtr create_player_bst_node(const char* name, const char* position, int age, float contractValue);
+PlayerBSTNodePtr insert_player_bst(PlayerBSTNodePtr root, PlayerBSTNodePtr newNode);
+void print_player_bst(const PlayerBSTNodePtr root);
+void test_player_bst_module();
+
+#endif
