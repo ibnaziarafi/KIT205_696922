f96a32f43d4d9258665bf67e8acbfeaed3fb7af2
ibna zia rafi <izrafi.au@gmail.com>
2025-05-20 08:39:01 +1000

    Milestone: Initial graph created and tested. Successfully implemented addEdge and printGraph functions.

--- Assignment_two_graphs/main.c
+++ Assignment_two_graphs/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "graph.h"
 
 int main() {
-    printf("Hello, World!\n");
+    // Create a graph with 5 vertices (0 to 4)
+    Graph* graph = createGraph(5);
+
+    // Add edges: from, to, weight
+    addEdge(graph, 0, 1, 10);
+    addEdge(graph, 0, 4, 20);
+    addEdge(graph, 1, 2, 30);
+    addEdge(graph, 1, 3, 40);
+    addEdge(graph, 2, 3, 50);
+    addEdge(graph, 3, 4, 60);
+
+    // Print the graph
+    printf("Graph structure:\n");
+    printGraph(graph);
+
+
     return 0;
 }
