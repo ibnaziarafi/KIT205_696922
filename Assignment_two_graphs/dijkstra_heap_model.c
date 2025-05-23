94ed6647299152bd3ddcc2d8a953e15aaccab91b
ibna zia rafi <izrafi.au@gmail.com>
2025-05-24 05:38:13 +1000

    added necessary functions for dijkstra(heap) moel.

--- Assignment_two_graphs/dijkstra_heap_model.c
+++ Assignment_two_graphs/dijkstra_heap_model.c
@@ -16,3 +16,53 @@ typedef struct {
     MinHeapNode** array;
 } MinHeap;
 
+
+MinHeapNode* newMinHeapNode(int v, int dist) {
+    MinHeapNode* node = (MinHeapNode*)malloc(sizeof(MinHeapNode));
+    node->vertex = v;
+    node->distance = dist;
+    return node;
+}
+
+MinHeap* createMinHeap(int capacity) {
+    MinHeap* heap = (MinHeap*)malloc(sizeof(MinHeap));
+    heap->pos = (int*)malloc(capacity * sizeof(int));
+    heap->size = 0;
+    heap->capacity = capacity;
+    heap->array = (MinHeapNode**)malloc(capacity * sizeof(MinHeapNode*));
+    return heap;
+}
+
+
+void swapMinHeapNode(MinHeapNode** a, MinHeapNode** b) {
+    MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+
+void minHeapify(MinHeap* heap, int i) {
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < heap->size &&
+        heap->array[left]->distance < heap->array[smallest]->distance)
+        smallest = left;
+
+    if (right < heap->size &&
+        heap->array[right]->distance < heap->array[smallest]->distance)
+        smallest = right;
+
+    if (smallest != i) {
+        MinHeapNode* smallestNode = heap->array[smallest];
+        MinHeapNode* iNode = heap->array[i];
+
+        heap->pos[smallestNode->vertex] = i;
+        heap->pos[iNode->vertex] = smallest;
+
+        swapMinHeapNode(&heap->array[smallest], &heap->array[i]);
+        minHeapify(heap, smallest);
+    }
+}
+
